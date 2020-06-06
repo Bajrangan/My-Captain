5cbc54e9a9dc72d8b0ae474d13aaf2c5d86c8669
Bajrangan <66505205+Bajrangan@users.noreply.github.com>
2020-06-06 02:00:48 -0700

    Add files via upload

---
+++ seperating positive and negative numbers in python.py
@@ -0,0 +1,18 @@
+
+
+a=list(map(int,input("input list: ").split(',')))
+print("positive numbers are:")
+for numbers in a:
+    if numbers>0:
+        print(numbers,end=' ')
+        
+          
+          
+
+ 
+
+                    
+
+
+
+      
