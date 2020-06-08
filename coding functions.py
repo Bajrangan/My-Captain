854007eb349a51f0e3a777b1a778ae779041c475
Bajrangan <66505205+Bajrangan@users.noreply.github.com>
2020-06-07 22:08:25 -0700

    Add files via upload

---
+++ coding functions.py
@@ -0,0 +1,16 @@
+#assigning element to different lists:
+
+list1=['google','yahoo','bing']
+list2=[23,45,67,78]
+
+#accessing elements from a tuple:
+
+tuple1=('ford','toyota','ferrari')
+print(tuple1[1])
+
+#deleting elements from dictionary:
+
+dictionary={1:"new york",2:"las vegas",3:"france"}
+del dictionary[3]
+print(dictionary)
+
